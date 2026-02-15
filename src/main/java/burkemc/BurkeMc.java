3931ece75fbcc6a6a70b09ad7d67d9ba148e350a
Caleb Burke <calebburke91@gmail.com>
2026-02-15 13:43:14 -0800

    dev env init + added ban for 2 days on death

---
+++ src/main/java/burkemc/BurkeMc.java
@@ -0,0 +1,56 @@
+package burkemc;
+
+import com.mojang.authlib.GameProfile;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.BannedPlayerEntry;
+import net.minecraft.server.BannedPlayerList;
+import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+public class BurkeMc implements ModInitializer {
+	public static final String MOD_ID = "burke-mc";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		ServerLivingEntityEvents.AFTER_DEATH.register((entity, source) -> {
+			if (entity instanceof ServerPlayerEntity player) {
+				var server = player.getEntityWorld().getServer();
+
+				if(server != null){
+					BannedPlayerList banList = server.getPlayerManager().getUserBanList();
+
+					long banDurationMs = 2L * 24 * 60 * 60 * 1000;	// 2 days
+					Date expiryDate = new Date(System.currentTimeMillis() + banDurationMs);
+					String reason = "You died! Banned for 2 days.";
+
+					var profile = player.getPlayerConfigEntry();
+
+					BannedPlayerEntry banEntry = new BannedPlayerEntry(profile, new Date(), "Death System", expiryDate, reason);
+
+					banList.add(banEntry);
+					player.networkHandler.disconnect(Text.literal(reason));
+
+					LOGGER.info("Banned player {} for 2 days due to death.", player.getEntity());
+				}
+			}
+		});
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
